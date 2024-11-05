5a3e29cd194235018e9abf555b6b6837580ebd79
agathasunyali <164965209+agathasunyali@users.noreply.github.com>
2024-11-05 10:08:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -253,3 +253,49 @@ var slideIndex = 0;
             alert('You have applied for a trial class');
             // Here you would handle the application for a trial class
         }
+
+
+
+
+
+
+
+ var slideIndex = 0;
+        showSlides();
+        
+        function showSlides() {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}    
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 3000); 
+        }
+    
+        function consult(type) {
+            alert('You have chosen to consult about: ' + type);
+            // Here you would typically handle the consultation logic, like opening a new window or modal
+        }
+    
+        function download(subject) {
+            alert('You have chosen to download: ' + subject + ' material package');
+            // Here you would handle the download logic
+        }
+    
+        function test(subject) {
+            alert('You have chosen to take: ' + subject + ' ability test');
+            // Here you would handle the testing logic
+        }
+    
+        function applyTrial() {
+            alert('You have applied for a trial class');
+            // Here you would handle the application for a trial class
+        }
